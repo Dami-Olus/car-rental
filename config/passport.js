4ed520f78ee38992a38f84f95d386e3c945d0feb
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-10 23:02:07 -0400

    setup passport config

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,46 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 //Require your User Model here!
+const UserModel = require("../models/user");
 
 // configuring Passport!
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in via OAuth!
-    // refer to the lesson plan from earlier today in order to set this up
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      // a user has logged in via OAuth!
+      // refer to the lesson plan from earlier today in order to set this up
+      try {
+        let userDocument = await UserModel.findOne({ googleId: profile.id });
 
-  }
-));
+        if (userDocument) return cb(null, userDocument);
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+        userDocument = await UserModel.create({
+          name: profile.displayName,
+          googelId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value,
+        });
+        return cb(null, userDocument);
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
 });
 
-passport.deserializeUser(function(id, done) {
-
+passport.deserializeUser(async function (id, done) {
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
-  // When you call this done function passport assigns the user document to req.user, which will 
+  const user = await UserModel.findById(id);
+  done(null, user);
+  // When you call this done function passport assigns the user document to req.user, which will
   // be availible in every Single controller function, so you always know the logged in user
-
 });
-
-
-
