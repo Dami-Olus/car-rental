8321547baf429e6cf7e1ab03a10d75a709ec3474
Damola Olusakin <olusakinadedamola@gmail.com>
2023-07-12 12:49:19 -0400

    user will not see own car in rental view

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -6,13 +6,20 @@ show
 }
 
 async function index(req,res){
-  const rentals = await Car.find({})
+  const rentals = await Car
+  .find({})
+  .populate('user') //  'cast' is the property name on the movie schema that has ref: 'Performer'
+  .exec();
+console.log(rentals[0].user._id.valueOf());
+
+  
+  
 res.render('rentals/index', {rentals: rentals})
 }
 
 async function show(req,res){
   const rental = await Car.findById(req.params.id);
   console.log(rental);
-  res.render('rentals/show', {rental: rental})
+  res.render('rentals/show', {rentals: rentals})
 
 }
